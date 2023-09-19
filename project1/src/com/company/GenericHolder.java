778d3e06a7f1a3918ccaa612f6616da897ea658c
igorekmniejniz3 <igor.wojcik@tm1.edu.pl>
2023-09-19 12:52:42 +0200

    dodanie klasy GenericHolder

--- project1/src/com/company/GenericHolder.java
+++ project1/src/com/company/GenericHolder.java
@@ -1,2 +1,17 @@
-package com.company;public class GenericHolder {
+package com.company;
+
+public class GenericHolder<AnyType> {
+    private AnyType a;
+
+    public GenericHolder(AnyType a) {
+        this.a = a;
+    }
+
+    public AnyType getA() {
+        return a;
+    }
+
+    public void setA(AnyType a) {
+        this.a = a;
+    }
 }
