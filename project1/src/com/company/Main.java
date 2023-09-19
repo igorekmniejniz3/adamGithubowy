d5046594cb9ad3c39b89ea02d9a64a1019f292cc
igorekmniejniz3 <igor.wojcik@tm1.edu.pl>
2023-09-19 13:16:15 +0200

    dodanie klasy object holder

--- project1/src/com/company/Main.java
+++ project1/src/com/company/Main.java
@@ -5,6 +5,16 @@ public class Main {
     public static void main(String[] args) {
         LiczbaHolder lh1 = new LiczbaHolder(12);
         LiczbaHolder lh2 = new LiczbaHolder(44);
-        System.out.println(lh1.getA());
+        System.out.println("lh1 = "+lh1.getA());
+        /////////////////////////////
+        GenericHolder gh1 = new GenericHolder(123);
+        System.out.println("gh1 = "+gh1.getA());
+        gh1.setA(444);
+        System.out.println("gh1 = "+gh1.getA());
+        ////////////////////////////
+        ObjectHolder oh1 = new ObjectHolder("dsadsa");
+        System.out.println("oh1 = "+oh1.getA());
+        oh1.setA(123);
+        System.out.println("oh1 = "+oh1.getA());
     }
 }
